527e3a5619e9037d78f2db3c86963d233bd3c481
tchanda <tchanda90@gmail.com>
2018-11-09 00:56:01 +0100

    minor changes

--- 01_import.R
+++ 01_import.R
@@ -3,21 +3,48 @@
 library(readr)
 library(dplyr)
 
+posts_names <- c("Id",
+  "PostTypeId",
+  "CreationDate",
+  "Score",
+  "ViewCount",
+  "Body",
+  "OwnerUserId",
+  "LastActivityDate",
+  "Title",
+  "Tags",
+  "AnswerCount",
+  "CommentCount",
+  "ParentId",
+  "AcceptedAnswerId",
+  "LastEditorUserId",
+  "LastEditDate",
+  "ClosedDate",
+  "FavoriteCount",
+  "OwnerDisplayName",
+  "LastEditorDisplayName",
+  "CommunityOwnedDate"
+)
+
+comments_names <- c("Id",
+                    "PostId",
+                    "Score",
+                    "Text",
+                    "CreationDate",
+                    "UserId",
+                    "UserDisplayName")
+
+
 start_time <- Sys.time()
 posts <- read_csv(paste0(csv_loc, "posts.csv"))
 comments <- read_csv(paste0(csv_loc, "comments.csv"))
-
-posts %>% head(1)
-comments %>% head(1)
 end_time <- Sys.time()
-
 time_taken <- difftime(end_time, start_time, units='mins')
-
 print(paste0("Time Taken for reading dataframe is ", time_taken))
 
 
 #select all the post ids
-post_ids <- as.numeric(paste(posts$Id))
+post_ids <- posts$Id
 
 #filter comments dataframe to contain rows that are also in posts dataframe
 comments <- filter(comments, PostId %in% post_ids)
