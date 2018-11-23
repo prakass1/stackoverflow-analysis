2b1c8273799ef657ba2ff91025207e0a308b398d
Subash Prakash <subashprakash8@gmail.com>
2018-11-23 20:56:56 +0100

    All changes for feature engineering - Small change to the missed commit

--- 02_preprocessing.R
+++ 02_preprocessing.R
@@ -1,6 +1,8 @@
 library("stringr")
 library("gsubfn")
 library("ggplot2")
+library("stringr")
+library("lubridate")
 #select all the post ids
 post_ids <- posts$Id
 #filter comments dataframe to contain rows that are also in posts dataframe
@@ -10,34 +12,135 @@ comments <- filter(comments, PostId %in% post_ids)
 questions <- filter(posts, PostTypeId == 1)
 answers <- filter(posts, PostTypeId == 2)
 
-#remove html tags from the body column
-questions <- RemoveHTML(questions)
-answers <- RemoveHTML(answers)
 
-max(tags$Count)
+## Use only selective columns
+question_cols_filtered <- c("Id",
+                            "PostTypeId",
+                            "CreationDate",
+                            "Score",
+                            "ViewCount",
+                            "Body",
+                            "LastActivityDate",
+                            "Title",
+                            "Tags",
+                            "AnswerCount",
+                            "FavoriteCount")
+
+answers_cols_filtered <- c("Id",
+                          "CreationDate",
+                          "Score",
+                          "Body",
+                          "CommentCount",
+                          "ParentId")
+
+
+
+
+
+
+#Selected Columns for question and answers
+questions_sel <- questions[question_cols_filtered]
+answers_sel <- answers[answers_cols_filtered]
+
+
+##Transform Date to yyyy/mm/dd format
+
+class(questions_sel$CreationDate)
+
+questions_sel <- questions_sel %>%
+                 transform(CreationDate = as.Date(CreationDate, tryFormats = c("%Y%m%d"))) %>%
+                 transform(LastActivityDate = as.Date(LastActivityDate, tryFormats = c("%Y%m%d")))
+
+answers_sel <- answers_sel %>%
+  transform(CreationDate = as.Date(CreationDate, tryFormats = c("%Y%m%d")))
+
+
+### Make all NA cols to 0 for not having any NULLS
+questions_sel[is.na(questions_sel)] <- 0
+
+
+#### Check <code> tag and add a new column called hasCode
+# hasCodeTag <- function(x) {
+#   
+#               if(grapl("<code>",x,fixed=TRUE)){
+#                   return(1)
+#               }
+#               else{
+#                 return(0)
+#               }
+#               }
+
+
+# Check and add a new column as hasCodeTag to see if there is already a code
+questions_sel <- questions_sel %>%
+                 mutate(hasCodeTag = case_when(grepl("<code>",Body,fixed=TRUE) == 1 ~ 1, TRUE ~ 0)) %>%
+                 mutate(hasLinks = case_when(grepl("http://",Body,fixed=TRUE) == 1 ~ 1, TRUE ~ 0))
+
+answers_sel <- answers_sel %>%
+                mutate(hasCodeTag = case_when(grepl("<code>",Body,fixed=TRUE) == 1 ~ 1, TRUE ~ 0)) %>%
+                mutate(hasLinks = case_when(grepl("http://",Body,fixed=TRUE) == 1 ~ 1, TRUE ~ 0))
+  
+### Extract links in the text
+
+
+
+
+
+#remove html tags from the body column
+#questions <- RemoveHTML(questions)
+#answers <- RemoveHTML(answers)
+
 
 ###Select only those 20 top tags by count########
-selected_tags_count <- tbl_df(tags) %>%
-                 top_n(25,Count) %>%
-                 select(TagName,Count) 
+# selected_tags_count <- tbl_df(tags) %>%
+#                  top_n(15,Count) %>%
+#                  select(TagName,Count) 
+# 
+# selected_tags_count <- arrange(selected_tags_count,desc(Count))
+# 
+# 
+# selected_tags <- selected_tags_count$TagName
 
-selected_tags_count <- arrange(selected_tags_count,desc(Count))
 
+#15 selected tags
+selected_tags <- c("android",
+                   "c",
+                   "c#",
+                   "c++",
+                   "html",
+                   "css",
+                   "ios",
+                   "java",
+                   "javascript",
+                   "jquery",
+                   "mysql",
+                   "php",
+                   "python",
+                   "r",
+                   "sql")
 
-selected_tags <- selected_tags_count$TagName
-X <- "<python><python3><unix>"
-
-X <- "python,python3,unix"
-
-#Choose from top 23 tags and generalize
 filterTags <- function(x){
             for(tag in selected_tags){
-            if(grepl(tag,x,fixed = TRUE)){
-              return(tag)
+              testExpr <- paste0("<",tag,">")
+            if(grepl(testExpr,x,fixed = TRUE)){
+              return(x)
             }
 
             }
-  return(x)
+  return("")
+}
+
+
+#### Filter as one tag -- This is purely for Visualizations ####
+filterOneTag <- function(x){
+  for(tag in selected_tags){
+    testExpr <- paste0("<",tag,">")
+    if(grepl(testExpr,x,fixed = TRUE)){
+      return(tag)
+    }
+    
+  }
+  return("")
 }
 
 #### Filter to have only the top 25 tags by questions######
@@ -45,9 +148,50 @@ filterTags <- function(x){
 questions["Tags"] <- apply(questions["Tags"],1,filterTags)
 #### Filter to only have tags and remove <> tags
 question_filtered <- questions %>%
-                    filter(!grepl("<", Tags))
+                    filter(grepl("<", Tags))
 
-
-
-# Very basic bar graph
+# Plot the frequency of Tags in the dataset
 ggplot(data=question_filtered, aes(x=Tags,fill=Tags)) + geom_bar(stat="count")
+
+
+
+
+
+
+##### Do one tag summarization
+
+questions["Tags"] <- apply(questions["Tags"],1,filterOneTag)
+questions <- questions %>% filter(Tags!="")
+
+val <- questions %>%
+       group_by(Tags) %>%
+       summarise(sum_answer_count = sum(AnswerCount),
+                 sum_comment_count = sum(CommentCount),
+                 sum_score = sum(Score)) %>%
+  #top_n(5,avg_answer_count) %>%
+       select(Tags,sum_answer_count,sum_comment_count,sum_score)
+
+# Plot answer_counts aggregated with Tags
+angle <- theme(axis.text.x = element_text(angle=60))
+pdf(file="plots_questions.pdf",paper = "a4" )
+ggplot(questions, aes(x=Tags,fill=Tags)) + 
+  geom_bar(stat = "count") + 
+  ggtitle("Frequency of Tags") + 
+  angle
+ggplot(val, aes(x=Tags,y=sum_answer_count,fill=sum_answer_count)) +
+  geom_bar(stat = "identity") +
+  ggtitle("Tag Distribution on Answers") +
+  angle
+ggplot(val, aes(x=Tags,y=sum_comment_count,fill=sum_answer_count)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle("Tag Distribution on Comment") +
+  angle
+ggplot(val, aes(x=Tags,y=sum_score,fill=sum_answer_count)) + 
+  geom_bar(stat = "identity") + 
+  ggtitle("Tag Distribution on Score") +
+  angle
+
+dev.off()
+graphics.off()
+
+
