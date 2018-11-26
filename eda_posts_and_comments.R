481a4e382a12ba8b1d6663e411543178c525677a
tchanda <tchanda90@gmail.com>
2018-11-27 00:58:21 +0100

    added separate files for eda

---
+++ eda_posts_and_comments.R
@@ -0,0 +1,17 @@
+# eda_posts_and_comments
+
+library("corrplot")
+
+
+score_questions <- questions %>%
+    arrange(desc(Score)) %>%
+    select(Score, HasCode, HasLinks, AnswerCount, CommentCount, FavoriteCount)
+
+scoresCol <- cor(score_questions)
+
+corrplot(scoresCol, method="color",  
+         type="lower", order="hclust", 
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=0, #Text label color and rotation
+         # hide correlation coefficient on the principal diagonal
+         diag=FALSE )
