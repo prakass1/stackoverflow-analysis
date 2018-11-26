1d4c1e802d17842c93e12f45072c47e9b41f48d1
tchanda <tchanda90@gmail.com>
2018-11-27 00:56:46 +0100

    some clean up added comments

--- 00_main.R
+++ 00_main.R
@@ -1,83 +1,89 @@
 # 00_main.R
 # A wrapper to run all R scripts
+
 library("tryCatchLog")
-#Source configurations first
+
+# source configurations first
 source("SO_config.R")
 
 xmls <- c()
 ne_csvs <- c()
 
-#check if csvs exist
+# check if csvs exist
 count <-0
 
-#source util.R
+# source util.R
 source("util.R")
 
 for(val in files){
-  print(paste0(csv_loc, val, ext))
-  if(file.exists(paste0(csv_loc, val, ext))){
-    count=count+1
-    #xmls <- append(xmls,val)
-  }
-  else{
-    ne_csvs <- append(ne_csvs, val)
-  }
+    print(paste0(csv_loc, val, ext))
+    if(file.exists(paste0(csv_loc, val, ext))){
+        count=count+1
+        # xmls <- append(xmls,val)
+    }
+    else{
+        ne_csvs <- append(ne_csvs, val)
+    }
 }
 
 ######## csv stuff ##################
 if(count!=4){
-  #call to csvs
-  start_time <- Sys.time()
-  for(val in ne_csvs){
-     #Assign each parsed vals to dataframe
-     print("begin")
-    # Attempt to use logging functionality: 
-    # https://cran.r-project.org/web/packages/tryCatchLog/vignettes/tryCatchLog-intro.html ##
-     tryLog(assign(paste0(val, "_df"), 
-                   ReadXMLToDf(paste0(xml_loc, val, ".xml"), 
-                   paste0("//", val, "/row"))))
-     print("end")
-     }
+    # call to csvs
+    start_time <- Sys.time()
+    for(val in ne_csvs){
+        # Assign each parsed vals to dataframe
+        print("begin")
+        # Attempt to use logging functionality: 
+        # https://cran.r-project.org/web/packages/tryCatchLog/vignettes/tryCatchLog-intro.html ##
+        tryLog(assign(paste0(val, "_df"), 
+                      ReadXMLToDf(paste0(xml_loc, val, ".xml"), 
+                                  paste0("//", val, "/row"))))
+        print("end")
+    }
 }
+
 end_time <- Sys.time()
 time_taken <- difftime(end_time, start_time, units='mins')
 print(paste0("Time Taken for creating dataframe is ", time_taken))
 
 
-#export as csv
+# export as csv
 print(paste0("Begin writing to csv ....."))
 for(val in ne_csvs){
-  if(exists(paste0(val,"_df"))){
-    dfs <- eval(parse(text=paste0(val, "_df")))
-    WriteToCsv(dfs, paste0(csv_loc, val, ext))
-  }
+    if(exists(paste0(val,"_df"))){
+        dfs <- eval(parse(text=paste0(val, "_df")))
+        WriteToCsv(dfs, paste0(csv_loc, val, ext))
+    }
 }
 print(paste0("Writing to csv completed ....."))
-#About 5min and 25 seconds
+# About 5min and 25 seconds
 
-#save some memory
+
+# save some memory
 if(exists('dfs')){
-  remove(dfs)
+    remove(dfs)
 }
 
 if(exists('posts_df')){
-  remove(posts_df)
+    remove(posts_df)
 }
 
 if(exists('comments_df')){
-  remove(comments_df)
+    remove(comments_df)
 }
 
 if(exists('tags_df')){
-  remove(tags_df)
+    remove(tags_df)
 }
 
 if(exists('users_df')){
-  remove(users_df)
+    remove(users_df)
 }
 
 
-#import csv to Data Frame
+# import csv to dataframe
 source('01_import.R')
-#Perform Preprocessing to Prepare Data
+
+# perform preprocessing to prepare data
 source('02_preprocessing.R')
+
--- 01_import.R
+++ 01_import.R
@@ -1,30 +1,30 @@
-#01_import.R
+# 01_import.R
+# import files into the workspace
 
-library(readr)
-library(dplyr)
+library(tidyverse)
 
 posts_names <- c("Id",
-  "PostTypeId",
-  "CreationDate",
-  "Score",
-  "ViewCount",
-  "Body",
-  "OwnerUserId",
-  "LastActivityDate",
-  "Title",
-  "Tags",
-  "AnswerCount",
-  "CommentCount",
-  "ParentId",
-  "AcceptedAnswerId",
-  "LastEditorUserId",
-  "LastEditDate",
-  "ClosedDate",
-  "FavoriteCount",
-  "OwnerDisplayName",
-  "LastEditorDisplayName",
-  "CommunityOwnedDate"
-)
+                 "PostTypeId",
+                 "CreationDate",
+                 "Score",
+                 "ViewCount",
+                 "Body",
+                 "OwnerUserId",
+                 "LastActivityDate",
+                 "Title",
+                 "Tags",
+                 "AnswerCount",
+                 "CommentCount",
+                 "ParentId",
+                 "AcceptedAnswerId",
+                 "LastEditorUserId",
+                 "LastEditDate",
+                 "ClosedDate",
+                 "FavoriteCount",
+                 "OwnerDisplayName",
+                 "LastEditorDisplayName",
+                 "CommunityOwnedDate"
+                 )
 
 comments_names <- c("Id",
                     "PostId",
@@ -32,16 +32,104 @@ comments_names <- c("Id",
                     "Text",
                     "CreationDate",
                     "UserId",
-                    "UserDisplayName")
+                    "UserDisplayName"
+                    )
 
+tags_names <- c("Id",
+                "TagName",
+                "Count",
+                "ExcerptPostId",
+                "WikiPostId"
+                )
 
-
+users_names <- c("Id",
+                 "Reputation",
+                 "CreationDate",
+                 "DisplayName",
+                 "LastAccessDate",
+                 "WebsiteUrl",
+                 "Location",
+                 "AboutMe",
+                 "Views",
+                 "UpVotes",
+                 "DownVotes",
+                 "ProfileImageUrl",
+                 "AccountId"
+)
 
 
 start_time <- Sys.time()
-posts <- read_csv(paste0(csv_loc, "posts.csv"))
-comments <- read_csv(paste0(csv_loc, "comments.csv"))
-tags <- read_csv(paste0(csv_loc, "tags.csv"))
+
+posts <- read_csv(paste0(csv_loc, "posts.csv"),
+                  col_names = TRUE,
+                  col_types = list(col_integer(), # Id
+                                   col_integer(), # PostTypeId
+                                   col_datetime(), # CreationDate
+                                   col_integer(), # Score
+                                   col_integer(), # ViewCount
+                                   col_character(), # Body
+                                   col_integer(), # OwnerUserId
+                                   col_datetime(), # LastActivityDate
+                                   col_character(), # Title
+                                   col_character(), # Tags
+                                   col_integer(), # AnswerCount
+                                   col_integer(), # CommentCount
+                                   col_integer(), # ParentId
+                                   col_integer(), # AcceptedAnswerId
+                                   col_integer(), # LastEditorUserId
+                                   col_datetime(), # LastEditDate
+                                   col_datetime(), # ClosedDate
+                                   col_integer(), # FavoriteCount
+                                   col_character(), # OwnerDisplayName
+                                   col_character(), # LastEditorDisplayName
+                                   col_datetime() # CommunityOwnedDate
+                                   )
+                  )
+
+
+comments <- read_csv(paste0(csv_loc, "comments.csv"),
+                     col_names = TRUE,
+                     col_types = list(col_integer(), # Id
+                                      col_integer(), # PostId
+                                      col_integer(), # Score
+                                      col_character(), # Text
+                                      col_datetime(), # CreationDate
+                                      col_integer(), # UserId
+                                      col_character() # UserDisplayName
+                                      )
+                    )
+
+
+tags <- read_csv(paste0(csv_loc, "tags.csv"),
+                 col_names = TRUE,
+                 col_types = list(col_integer(), # Id
+                                  col_character(), # TagName
+                                  col_integer(), # Count
+                                  col_integer(), # ExcerptPostId
+                                  col_integer() # WikiPostId
+                                  )
+                 )
+
+
+users <- read_csv(paste0(csv_loc, "users.csv"),
+                  col_names = TRUE,
+                  col_types = list(col_integer(), # Id
+                                   col_integer(), # Reputation
+                                   col_datetime(), # CreationDate
+                                   col_character(), # DisplayName
+                                   col_datetime(), # LastAccessDate
+                                   col_character(), # WebsiteUrl
+                                   col_character(), # Location
+                                   col_character(), # AboutMe
+                                   col_integer(), # Views
+                                   col_integer(), # UpVotes
+                                   col_integer(), # DownVotes
+                                   col_character(), # ProfileImageUrl
+                                   col_integer() # AccountId
+                                   )
+                  )
+
+
 end_time <- Sys.time()
 time_taken <- difftime(end_time, start_time, units='mins')
-print(paste0("Time Taken for reading dataframe is ", time_taken))
+print(paste0("Time Taken for reading dataframes: ", time_taken))
