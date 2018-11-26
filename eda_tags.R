041037e3c75b339add95c3f064035cedabf9e872
tchanda <tchanda90@gmail.com>
2018-11-27 00:27:26 +0100

    added eda file for tags

---
+++ eda_tags.R
@@ -0,0 +1,45 @@
+#eda_tags.R
+
+
+# apply the filterOneTag function on the Tags column of questions
+# this will make the Tags column have only one tag
+questions_one_tag <- questions
+questions_one_tag["Tags"] <- apply(questions["Tags"], 1, filterOneTag)
+questions_one_tag <- questions_one_tag %>% filter(Tags != "")
+
+
+# questions grouped by total answer count, comment count, and sum of scores of tags
+tags_summary <- questions_one_tag %>%
+    group_by(Tags) %>%
+    summarise(sum_answer_count = sum(AnswerCount),
+              sum_comment_count = sum(CommentCount),
+              sum_score = sum(Score)) %>%
+    #top_n(5,avg_answer_count) %>%
+    select(Tags, sum_answer_count, sum_comment_count, sum_score)
+
+
+
+# Plot answer_counts aggregated with Tags
+angle <- theme(axis.text.x = element_text(angle=60))
+pdf(file="plots_questions.pdf",paper = "a4" )
+
+ggplot(questions_one_tag, aes(x=Tags,fill=Tags)) + 
+    geom_bar(stat = "count") + 
+    ggtitle("Frequency of Tags") + 
+    angle
+ggplot(tags_summary, aes(x=Tags,y=sum_answer_count,fill=sum_answer_count)) +
+    geom_bar(stat = "identity") +
+    ggtitle("Tag Distribution on Answers") +
+    angle
+ggplot(tags_summary, aes(x=Tags,y=sum_comment_count,fill=sum_comment_count)) + 
+    geom_bar(stat = "identity") + 
+    ggtitle("Tag Distribution on Comment") +
+    angle
+ggplot(tags_summary, aes(x=Tags,y=sum_score,fill=sum_score)) + 
+    geom_bar(stat = "identity") + 
+    ggtitle("Tag Distribution on Score") +
+    angle
+
+dev.off()
+graphics.off()
+
