65dc34b67cab67f299686bb7af26c6c6eca17e59
tchanda <tchanda90@gmail.com>
2018-12-08 05:00:50 +0100

    updates

--- SO_config.R
+++ SO_config.R
@@ -1,8 +1,35 @@
 # SO_config.R
 # StackOverflow Configurations
 
-csv_loc <- "F:/csv_location/"
-xml_loc <- "F:/xml_locations/"
+library("dplyr")
+library("XML")
+library("tm")
+library("textmineR")
+library("gsubfn")
+library("SnowballC")
+library("textstem")
+library("tidytext")
+library("tm")
+library("ggmap")
+library("tidytext")
+library("sentimentr")
+library("wordcloud")
+library("gender")
+library("corrplot")
+library("stringr")
+library("gsubfn")
+library("ggplot2")
+library("stringr")
+library("lubridate")
+library("rvest")
+library("corrplot")
+library("magrittr")
+library("shiny")
+library("leaflet")
+
+
+xml_loc <- "data/"
+csv_loc <- "data/"
 
 ### For Other OS ############
 # xml_loc <- "/data/"
